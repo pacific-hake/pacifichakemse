8bc97e416c708007907263a0b7143703b85c7b20
Chris Grandin <chris.grandin@gmail.com>
2020-05-01 14:46:46 -0700

    Add new file for setting up plotting objects

--- R/setup_mse_plot_objects.R
+++ R/setup_mse_plot_objects.R
@@ -1,4 +1,4 @@
-#' Setup MSE plot data frames and colors
+#' Setup MSE plot data frames, colors, and plot names
 #'
 #' @details Reads in the `.rds` files found in the `results_dir` directory and
 #' creates two indicator [data.frame]s and colors for plotting
@@ -6,8 +6,7 @@
 #' @param results_dir A directory name where results from [run_mses()] resides. Only files
 #' in this directory that end in '.rds' and contain 'MSE' will be loaded
 #' @param plotnames Names for the plots
-#' @param plotexp See [fn_plot_MSE()]
-#' @param pidx See [fn_plot_MSE()]
+#' @param porder Order of the scenarios in the figures. Default is the order they appear in the
 #'
 #' @return A list of length 4: The items are (1) A [data.frame] containing the SSB/AAV/Catch
 #' indicators, (2) A [data.frame] containing the country and season indicators, (3) A [data.frame]
@@ -28,12 +27,15 @@
 #' @export
 setup_mse_plot_objects <- function(results_dir = NULL,
                                    plotnames = NULL,
-                                   plotexp = FALSE,
-                                   pidx = NA){
+                                   porder = NA){
   stopifnot(!is.null(results_dir))
 
   fls <- dir(results_dir)
   fls <- fls[grep("\\.rds", fls)]
+  if(!length(fls)){
+    stop("There are no .rds files in the '", results_dir, "' directory",
+         call. = FALSE)
+  }
   fls <- fls[grep("MSE", fls)]
   fls <- map_chr(fls, ~{
     file.path(results_dir, .x)
@@ -65,9 +67,10 @@ setup_mse_plot_objects <- function(results_dir = NULL,
   sim_data <- run.agebased.true.catch(df)
   names(ls_plots) <- plotnames
 
-  if(all(is.na(pidx))){
-    pidx <- 1:length(plotnames)
+  if(all(is.na(porder))){
+    porder <- 1:length(plotnames)
   }
+  stopifnot(length(porder) == length(ls_plots))
 
   #cols <- brewer.pal(6, "Dark2")
   cols <- pnw_palette("Starfish", n = 4, type = "discrete")
@@ -86,7 +89,8 @@ setup_mse_plot_objects <- function(results_dir = NULL,
   })
   df_violin_indicators <- map_df(lst_indicators, ~{
     .x[[3]]
-  })
+  }) %>%
+    mutate(HCR = factor(HCR, levels = names(lst_indicators)[porder]))
   violin_names <- names(df_violin_indicators)
   violin_last_word <- str_extract(violin_names, "\\w+$")
   season_inds <- grep("sp|su|fa", violin_last_word)
@@ -102,7 +106,7 @@ setup_mse_plot_objects <- function(results_dir = NULL,
   cutoff_ind <- grep("long term", indicators)
   df_ssb_catch_indicators <- df_all_indicators %>%
     filter(indicator %in% indicators[1:cutoff_ind]) %>%
-    mutate(HCR = factor(HCR, levels = names(lst_indicators)[pidx]))
+    mutate(HCR = factor(HCR, levels = names(lst_indicators)[porder]))
 
   df_country_season_indicators <- df_all_indicators %>%
     filter(indicator %in% indicators[(cutoff_ind + 1):length(indicators)]) %>%
@@ -112,7 +116,8 @@ setup_mse_plot_objects <- function(results_dir = NULL,
                            "Apr-Jun",
                            ifelse(season == "sum",
                                   "July-Sept",
-                                  "Oct-Dec")))
+                                  "Oct-Dec"))) %>%
+    mutate(HCR = factor(HCR, levels = names(lst_indicators)[porder]))
 
   df_violin <- map_df(seq_along(ls_plots), ~{
     tmp <- hake_violin(ls_plots[[.x]],
@@ -121,7 +126,7 @@ setup_mse_plot_objects <- function(results_dir = NULL,
     tmp$HCR <- plotnames[.x]
     tmp
   }) %>%
-    mutate(HCR = factor(HCR, levels = plotnames[pidx]))
+    mutate(HCR = factor(HCR, levels = plotnames[porder]))
 
   list(ssb_catch_indicators = df_ssb_catch_indicators,
        country_season_indicators = df_country_season_indicators,
