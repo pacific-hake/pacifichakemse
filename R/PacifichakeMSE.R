475caf7a59d6971a01babee2b6ff1459db05965a
Chris Grandin <chris.grandin@gmail.com>
2020-05-01 14:25:15 -0700

    Changes to how the C++ Dlls are dealt with

--- R/PacifichakeMSE.R
+++ R/PacifichakeMSE.R
@@ -1,18 +1,12 @@
 #' Pacific hake MSE package
 #'
-#' \tabular{ll}{ Package: \tab PacifichakeMSE\cr Type: \tab Package\cr Version: \tab
-#' 1.0\cr Date: \tab 2020-02-26\cr License: \tab GPL-3\cr LazyLoad: \tab
-#' yes\cr URL: \tab \url{https://github.com/PacifichakeMSE/}\cr }
-#'
-#' @name PacifichakeMSE-package
+#' @name PacifichakeMSE
 #' @docType package
 #' @author Nis S Jacobsen, Ian G. Taylor, Kristin Marshall, Aaron Berger
 #' Package maintainer: Nis Jacobsen <nissandjac@@gmail.com>
 #' @references PacifichakeMSE on GitHub: \url{https://github.com/PacifichakeMSE}
-#' @import dplyr
-#'
+#' @importFrom Rcpp sourceCpp
 #' @examples
-#'
 #' \dontrun{
 #' # Run the MSE
 #' Fill in stuff
