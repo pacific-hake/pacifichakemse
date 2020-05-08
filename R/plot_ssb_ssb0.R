ef9ce5602ff618d3ba948e6d4bb96e1042b5eb04
Chris Grandin <chris.grandin@gmail.com>
2020-05-08 02:12:10 -0700

    Fix plot_ssb_ssb0() and add tests

--- R/plot_ssb_ssb0.R
+++ R/plot_ssb_ssb0.R
@@ -2,33 +2,48 @@
 #' Estimation model and SSB0 is the initial SSB from teh Operating model
 #'
 #' @param ps A plot setup object as output by [setup_mse_plot_objects()]
+#' @param ci A vector of length two of the lower and upper credible interval values.
+#' These values must have been calculated in [df_lists()] and exist in the data in `ps`
 #'
 #' @return A [ggplot2::ggplot()] object
 #' @importFrom dplyr ungroup
 #' @export
-plot_ssb_ssb0 <- function(ps = NULL){
+plot_ssb_ssb0 <- function(ps = NULL,
+                          ci = c(0.05, 0.95)){
 
   stopifnot(!is.null(ps))
+  stopifnot(!is.null(ci))
 
   sim_data <- ps$sim_data
+  ssb0 <- sum(sim_data$SSB0)
+  ssb <- ps$mse_values_agg$ssb_tot_quant
+  stopifnot("0.5" %in% names(ssb))
+  stopifnot(is.numeric(ci))
+  stopifnot(all(ci %in% names(ssb)))
+  stopifnot(length(ci) == 2)
 
-  ssbtot <- ps$mse_values_agg$ssbtot %>%
-    mutate(med = med / (sum(sim_data$SSB0))) %>%
-    mutate(p5 = p5 / (sum(sim_data$SSB0))) %>%
-    mutate(p95 = p95 / (sum(sim_data$SSB0)))
+  ssb_names <- names(ssb)
+  suppressWarnings(quant_names <- ssb_names[!is.na(as.numeric(ssb_names))])
+  if("avg" %in% names(ssb)){
+    quant_names <- c(quant_names, "avg")
+  }
+  ssb_ssb0 <- ssb %>%
+    mutate_at(vars(quant_names), function(x){x / ssb0})
 
-  g <- ggplot(ssbtot, aes(x = year, y = med, color = run, fill = run))+
-    geom_line(size = 1.5)+
-    geom_line(aes(y = p95), linetype =2, size = 1.2)+
-    geom_line(aes(y = p5), linetype =2, size = 1.2)+
-    scale_color_manual(values = ps$cols)+
+  ci <- as.character(ci) %>% map(~{sym(.x)})
+
+  g <- ggplot(ssb_ssb0, aes(x = year, y = `0.5`, color = run, fill = run)) +
+    geom_line(size = 1.5) +
+    geom_line(aes(y = !!ci[[1]]), linetype = 2, size = 1.2) +
+    geom_line(aes(y = !!ci[[2]]), linetype = 2, size = 1.2) +
+    scale_color_manual(values = ps$cols) +
     theme_classic() +
-    scale_y_continuous(name ="Total SSB/SSB0")+
-    coord_cartesian(ylim  = c(0,4))+
+    scale_y_continuous(name ="Total SSB/SSB0") +
+    coord_cartesian(ylim  = c(0, 4)) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
           legend.position = c(0.2,.8),
-          legend.title = element_blank())+
-    scale_fill_manual(values = alpha(ps$cols, alpha = 0.2))+
+          legend.title = element_blank()) +
+    scale_fill_manual(values = alpha(ps$cols, alpha = 0.2)) +
     geom_hline(aes(yintercept = 1), color = "black", linetype = 2)
   g
 }
---
+++ tests/testthat/test-plot-ssb-ssb0.R
@@ -0,0 +1,40 @@
+context("Test the plot_ssb_ssb0() function")
+
+ps <- NULL
+ps$cols <- PNWColors::pnw_palette("Starfish", n = 4, type = "discrete")
+ps$sim_data$SSB0 <- c(472000, 1417000)
+ps$mse_values_agg$ssb_tot_quant <- tibble::tibble(
+  year = rep(c(2011, 2012, 2013), 4),
+  `0.05` = rep(c(880000, 820000, 885000), 4),
+  `0.25` = rep(c(890000, 810000, 895000), 4),
+  `0.5` = rep(c(900000, 820000, 805000), 4),
+  `0.75` = rep(c(910000, 840000, 925000), 4),
+  `0.95` = rep(c(950000, 870000, 965000), 4),
+  avg = rep(c(900000, 920000, 855000), 4),
+  run = c(rep("Base scenario", 3),
+          rep("Historical TAC", 3),
+          rep("Realized", 3),
+          rep("Floor 50", 3)))
+
+test_that("plot_ssb_ssb0() - Tests for argument errors", {
+  expect_error(plot_ssb_ssb0(ps = NULL, ci = c(0.05, 0.95)))
+  expect_error(plot_ssb_ssb0(ps = ps, ci = NULL))
+  expect_error(plot_ssb_ssb0(ps = ps, ci = c(0.05, 0.94)))
+  expect_error(plot_ssb_ssb0(ps = ps, ci = c("a", "b")))
+  expect_error(plot_ssb_ssb0(ps = ps, ci = c(0.05, 0.5, 0.95)))
+  ps_no_median <- ps
+  tmpnames <- names(ps$mse_values_agg$ssb_tot_quant)
+  tmpnames[4] <- "notright"
+  names(ps_no_median$mse_values_agg$ssb_tot_quant) <- tmpnames
+  expect_error(plot_catch(ps = ps_no_median,
+                          ci = c(0.05, 0.95)))
+
+})
+
+test_that("plot_ssb_ssb0() - Tests for plots matching previous ones", {
+  p <- plot_ssb_ssb0(ps = ps, ci = c(0.05, 0.95))
+  vdiffr::expect_doppelganger("plot-ssb-ssb0", p)
+  # Use following command to add new plots
+  # vdiffr::manage_cases()
+})
+
