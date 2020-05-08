83b72aaafee2be833729c818758892fc427025f1
Chris Grandin <chris.grandin@gmail.com>
2020-05-08 10:03:36 -0700

    Fix plot_standard_error() and add tests

--- R/plot_standard_error.R
+++ R/plot_standard_error.R
@@ -2,30 +2,33 @@
 #' the Estimation model
 #'
 #' @param ps A plot setup object as output by [setup_mse_plot_objects()]
-#' @param quants A vector of quantiles to pass to [stats::quantile()]. Median is also
-#' calculated
+#' #' @param ci A vector of length two of the lower and upper credible interval values.
+#' These values will be passed to [stats::quantile()]
 #'
 #' @return A [ggplot2::ggplot()] object
 #' @export
 plot_standard_error <- function(ps = NULL,
-                                quants = c(0.05, 0.95)){
+                                ci = c(0.05, 0.95)){
   stopifnot(!is.null(ps))
-  stopifnot(!is.null(quants[1]))
-  stopifnot(length(quants) == 2)
+  stopifnot(!is.null(ci))
 
-  se <- ps$standard_error_ssb %>%
-    group_by(scenario, year) %>%
-    summarise(lower = quantile(SE.SSB, quants[1], na.rm = TRUE),
-              mid = median(SE.SSB, na.rm = TRUE),
-              upper = quantile(SE.SSB, quants[2], na.rm = TRUE))
+  se <- ps$standard_error_ssb
+  stopifnot("0.5" %in% names(se))
+  stopifnot(is.numeric(ci))
+  stopifnot(all(ci %in% names(se)))
+  stopifnot(length(ci) == 2)
 
-  g <- ggplot(se, aes(x = year, y = lower)) +
+  ci <- as.character(ci) %>% map(~{sym(.x)})
+
+  g <- ggplot(se, aes(x = year, y = `0.5`)) +
     theme_classic() +
     geom_line(size = 1.5) +
-    facet_wrap(~scenario) +
+    facet_wrap(~run) +
     geom_hline(yintercept = 0.0, linetype = 2) +
-    geom_ribbon(aes(ymin = lower, ymax = upper),
-                fill = alpha("gray", alpha = 0.5)) +
+    geom_ribbon(aes(ymin = !!ci[[1]],
+                    ymax = !!ci[[2]]),
+                fill = alpha("gray",
+                             alpha = 0.5)) +
     scale_y_continuous(name = "Standard error")
   g
 }
