3c09ebaaa79a346041148b0540a0dd07d74455bb
Chris Grandin <chris.grandin@gmail.com>
2020-10-06 22:26:09 -0700

    Add pacifichakemse.R

---
+++ R/pacifichakemse.R
@@ -0,0 +1,10 @@
+#' Pacific Hake MSE package
+#'
+#' @name pacifichakemse
+#' @description Management strategy evaluation framework with a Spatial Operating Model.
+#' @docType package
+#' @author Nis S Jacobsen, Ian G. Taylor, Kristin Marshall, Aaron Berger, Chris Grandin
+#' Package maintainer: Nis Jacobsen <nissandjac@@gmail.com>
+#' @references PacifichakeMSE on GitHub: \url{https://github.com/nissandjac/PacifichakeMSE}
+#' @importFrom Rcpp sourceCpp
+NULL
