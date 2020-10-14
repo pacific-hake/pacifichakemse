9f5a5843cf4d8f2fcad300550b92ca03925e7619
Chris Grandin <chris.grandin@gmail.com>
2020-10-14 02:20:44 -0700

    Add hake thee so plots are the same as the assessment

---
+++ R/theme.R
@@ -0,0 +1,19 @@
+#' ggplot2 theme for hake
+#'
+#' @export
+#' @importFrom ggplot2 theme theme_bw element_rect element_blank element_line margin unit alpha theme_set
+hake_theme <- function(){
+  theme_bw() +
+    theme(legend.box.background = element_rect(fill = alpha("white", 0.7)),
+          legend.box.margin = margin(1, 1, 1, 1, "mm"),
+          legend.key = element_blank(),
+          legend.margin = margin(),
+          legend.text.align = 1,
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          #panel.grid.major = element_line(colour = "darkgrey", size = 0.2),
+          #panel.grid.minor = element_line(colour = "darkgrey", size = 0.1),
+          legend.background = element_rect(fill = "transparent"),
+          #panel.spacing.x=unit(3, "lines"),
+          plot.margin = unit(c(0.1, 0.6, 0.1, 0.1), "lines"))
+}
