7e31227d148cbd8e69e7b27b2533d33104611315
Chris Grandin <chris.grandin@gmail.com>
2020-11-10 01:37:08 -0800

    Fix ordering of coloring of facet backgrounds

--- R/utilities.R
+++ R/utilities.R
@@ -872,7 +872,7 @@ color_facet_backgrounds <- function(g = NULL,
 
   # Add scenario colors to the strip backgrounds
   gt <- ggplot_gtable(ggplot_build(g))
-  strip <- which(grepl('strip-', gt$layout$name))
+  strip <- which(grepl("strip-t", gt$layout$name))
   if(!length(strip)){
     warning("The ggplot object does not contain facets, returning the original object")
     return(g)
@@ -880,6 +880,23 @@ color_facet_backgrounds <- function(g = NULL,
   if(length(facet_back_cols) < length(strip)){
     warning("The facet_back_cols vector is shorter than the number of facet backgrounds. Recycling the colors")
     facet_back_cols <- rep(facet_back_cols, length(strip) %/% length(facet_back_cols) + 1)
+  }
+  # Need to re-order gt$layout$name[strip] as they are out of order and have the format strip-t-1-2 etc
+  # This is accomplished by reversing the numbers in the ordered strip text eg. strip-t-1-2 becomes strip-t-2-1
+  # If this swapping leads to strip text which doesn't exist in the grob layout, use the original text instead.
+  swap_rows_cols <- function(vec){
+    out <- NULL
+    for(i in seq_along(vec)){
+      out[i] <- gsub("strip-t-([1-9]+)-([1-9]+)", "strip-t-\\2-\\1", vec[i])
+    }
+    out
+  }
+  tmp_layout_name <- sort(gt$layout$name[strip])
+  strip_tmp <- match(tmp_layout_name, gt$layout$name)
+  tmp_layout_name <- swap_rows_cols(tmp_layout_name)
+  strip <- match(tmp_layout_name, gt$layout$name)
+  if(any(is.na(strip))){
+    strip <- strip_tmp
   }
   facet_back_cols <- facet_back_cols[seq_along(strip)]
 
