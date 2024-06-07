94bd2a44a7cef63e896df7df961a8a3e7fa89ff4
Chris Grandin <chris.grandin@gmail.com>
2024-06-07 09:56:59 -0700

    Add function to print more rows of data frames quickly

---
+++ R/utils-pr.R
@@ -0,0 +1,12 @@
+#' Wrapper for the `print()` function
+#'
+#' @param d The data frame
+#' @param num_rows The number of rows to show on screen
+#' @param ... Arguments passed to `print()`
+#'
+#' @return
+#' @export
+pr <- function(d, num_rows = 100, ...){
+  d |>
+    print(n = num_rows, ...)
+}
